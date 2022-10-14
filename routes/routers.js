ae8ad4cc06d8dd744a8ccfd4bde90ef7969d4c45
saikat-hiehq <saikat@hiehq.com>
2022-10-14 23:32:38 +0530

    some changes

--- routes/routers.js
+++ routes/routers.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const multer  = require('multer');
-const upload = multer({ dest: './public/uploads/' });
+const maxSize = 2 * 1024 * 1024;
+const upload = multer({ dest: './public/uploads/',
+limits: { fileSize: maxSize },
+fileFilter: (req, file, cb) => {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+  } 
+});
 
 const Controller = require('../controllers/Controller');
 
